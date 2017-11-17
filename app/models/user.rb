b5a488e24bd7bd35c88e74c408fa8b173888f4ce
Matt Conway <matt@conwaysplace.com>
2017-11-16 21:46:24 -0500

    added basic auth

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
+
+  include DeviseTokenAuth::Concerns::User
+
+end
