89fc29fa235464457ebaf02c6b68230915aff852
Jay Byoun <jay@jaybyoun.com>
2017-12-05 16:51:46 -0800

    add attributes to organizations

---
+++ db/migrate/20171206002616_add_attributes_to_organizations.rb
@@ -0,0 +1,14 @@
+class AddAttributesToOrganizations < ActiveRecord::Migration[5.1]
+  def change
+    add_column :organizations, :street_1, :string
+    add_column :organizations, :street_2, :string
+    add_column :organizations, :city, :string
+    add_column :organizations, :state, :string
+    add_column :organizations, :postal_code, :string
+    add_column :organizations, :country, :string
+    add_column :organizations, :phone, :string
+    add_column :organizations, :phone_ext, :string
+    add_column :organizations, :email, :string
+    add_column :organizations, :url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117151828) do
+ActiveRecord::Schema.define(version: 20171206002616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 20171117151828) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "street_1"
+    t.string "street_2"
+    t.string "city"
+    t.string "state"
+    t.string "postal_code"
+    t.string "country"
+    t.string "phone"
+    t.string "phone_ext"
+    t.string "email"
+    t.string "url"
   end
 
   create_table "users", force: :cascade do |t|
