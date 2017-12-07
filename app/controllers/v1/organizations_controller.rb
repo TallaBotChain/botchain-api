b581f047d40bac57fd7103bb8034e416e1d38935
Jay Byoun <jay@jaybyoun.com>
2017-12-07 05:29:54 -0800

    remove duplicate org controller

--- app/controllers/v1/organizations_controller.rb
+++ app/controllers/v1/organizations_controller.rb
@@ -1,3 +1,15 @@
-class OrganizationsController < ApplicationController
-  
+module V1
+  class OrganizationsController < ApplicationController
+    def show
+      @organization = Organization.find(params[:id])
+      render json: @organization
+    end
+
+    def update
+    end
+
+    def create
+    end
+
+  end
 end
