493bf00f819caf3da46f7150bace8107b8858c4f
Matt Conway <matt@conwaysplace.com>
2017-12-13 11:00:19 -0500

    seeds only in dev and idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,18 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 
-user = User.new(email: 'botchain@talla.com', nickname: 'Bot', name: 'Bot Chain', password: "botchain")
-user.skip_confirmation!
-user.save!
+if ! Rails.env.production? && ! Rails.env.test?
 
-org = Organization.create!(name: "Hollywood", description: "Bots galore!")
-org.bots.create!(name: "Bender", description: "beep boop")
-org.bots.create!(name: "Robby", description: "beep boop")
+  if User.count == 0
+    user = User.new(email: 'botchain@talla.com', nickname: 'Bot', name: 'Bot Chain', password: "botchain")
+    user.skip_confirmation!
+    user.save!
 
-OrganizationMember.create!(organization: org, user: user)
+    org = Organization.create!(name: "Hollywood", description: "Bots galore!")
+    org.bots.create!(name: "Bender", description: "beep boop")
+    org.bots.create!(name: "Robby", description: "beep boop")
+
+    OrganizationMember.create!(organization: org, user: user)
+  end
+
+end
