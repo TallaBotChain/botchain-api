5854e2b98732e43fada48cabdcc6cc0ea3f14619
Matt Conway <matt@conwaysplace.com>
2017-12-18 13:04:10 -0500

    fix running/testing through docker-compose

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,18 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 
-if ! Rails.env.production? && ! Rails.env.test?
-
-  if User.count == 0
-    user = User.new(email: 'botchain@talla.com', nickname: 'Bot', name: 'Bot Chain', password: "botchain")
-    user.skip_confirmation!
-    user.save!
-
-    org = Organization.create!(name: "Hollywood", description: "Bots galore!")
-    org.bots.create!(name: "Bender", description: "beep boop")
-    org.bots.create!(name: "Robby", description: "beep boop")
-
-    OrganizationMember.create!(organization: org, user: user)
-  end
-
-end
+# if ! Rails.env.production? && ! Rails.env.test?
+#
+#   if User.count == 0
+#     user = User.new(email: 'botchain@talla.com', nickname: 'Bot', name: 'Bot Chain', password: "botchain")
+#     user.skip_confirmation!
+#     user.save!
+#
+#     org = Organization.create!(name: "Hollywood", description: "Bots galore!")
+#     org.bots.create!(name: "Bender", description: "beep boop")
+#     org.bots.create!(name: "Robby", description: "beep boop")
+#
+#     OrganizationMember.create!(organization: org, user: user)
+#   end
+#
+# end
