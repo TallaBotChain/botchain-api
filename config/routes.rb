88cfb618da54b5ac75c0b3192d9840108d4ad730
Abliamit Abliamitov <ablyamyt@gmail.com>
2018-01-09 11:08:56 +0300

    Bot list API.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
   get "/access_token" => "auth#access_token", as: :access_token
-  
-  namespace :v1 do 
+
+  namespace :v1 do
 
     resources :developer_records, only: [:create]
-    resource :developer_records, only: [:show, :update] 
+    resource :developer_records, only: [:show, :update]
 
-    resources :bots, only: [:create]
-    resource :bots, only: [:show, :update] do 
+    resources :bots, only: [:create, :index]
+    resource :bots, only: [:show, :update] do
       put :transfer
       patch :transfer
     end
