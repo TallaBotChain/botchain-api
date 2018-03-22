69a82c1bb4cdc2312b283daa8594c79d1fd654bb
Jay Byoun <jay@jaybyoun.com>
2018-03-22 09:24:47 -0700

    add bot developer eth address sync

--- app/workers/bot_sync_worker.rb
+++ app/workers/bot_sync_worker.rb
@@ -12,13 +12,17 @@ class BotSyncWorker
     bot_count = contract.call.total_supply
     0..bot_count do |index|
       bot_url = contract.call.bot_entry_url(index)
-      bot = JSON.parse(RestClient.get(bot_url))
-      existing_bot = Bot.find_by(eth_address: bot['eth_address'])
+      bot_developer_eth_address = contract.call.get_bot_entry.andand.first
+      bot_metadata = JSON.parse(RestClient.get(bot_url))
+      existing_bot = Bot.find_by(eth_address: bot_metadata['eth_address'])
+      existing_bot_developer = Developer.find_by(eth_address: bot_developer_eth_address)
       if existing_bot.present?
-        existing_bot.update(bot)
+        existing_bot.update(bot_metadata)
+        existing_bot.update(developer: existing_bot_developer)
         existing_bot.update(metadata_url: bot_url)
       else
-        new_bot = Bot.create(bot)
+        new_bot = Bot.create(bot_metadata)
+        new_bot.update(developer: existing_bot_developer)
         new_bot.update(metadata_url: bot_url)
       end
     end
