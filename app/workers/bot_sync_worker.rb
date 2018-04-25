48b189e4791ca14d3de4cc9cd013cd09d26196ee
Jay Byoun <jay@jaybyoun.com>
2018-04-25 10:21:48 -0400

    updates to bot sync worker

--- app/workers/bot_sync_worker.rb
+++ app/workers/bot_sync_worker.rb
@@ -13,22 +13,24 @@ class BotSyncWorker
     (1..bot_count).each do |index|
       bot_url = contract.call.bot_entry_url(index)
       next if bot_url.empty?
-      bot_developer_eth_address = "0x#{contract.call.get_bot_entry(index).andand.first}"
+      bot_developer_eth_address = "0x#{contract.call.get_bot_entry(index).andand[0]}"
+      bot_eth_address = "0x#{contract.call.get_bot_entry(index).andand[1]}"
       begin
         bot_metadata = JSON.parse(RestClient.get(bot_url))
       rescue
         next
       end
       bot_metadata.select! {|k, v| Bot.column_names.include?(k) }
+      bot_metadata["bot_tags"] = bot_metadata["bot_tags"].split(/\s*,\s*/)
       existing_bot = Bot.find_by(eth_address: contract.call.get_bot_entry(index).andand[1])
       existing_bot_developer = Developer.find_or_create_by(eth_address: bot_developer_eth_address)
       if existing_bot.present?
         existing_bot.update(bot_metadata)
-        existing_bot.update(developer_id: existing_bot_developer.id) 
+        existing_bot.update(developer_id: existing_bot_developer.id, eth_address: bot_eth_address)
         existing_bot.update(metadata_url: bot_url)
       else
         new_bot = Bot.create!(bot_metadata)
-        new_bot.update(developer_id: existing_bot_developer.id)
+        new_bot.update(developer_id: existing_bot_developer.id, eth_address: bot_eth_address)
         new_bot.update(metadata_url: bot_url)
       end
     end
