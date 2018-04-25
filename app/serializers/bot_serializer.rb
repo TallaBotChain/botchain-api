5e9f8f395ceff9c0122043b7d82a920f35f37301
Jay Byoun <jay@jaybyoun.com>
2018-04-24 23:59:04 -0400

    remove hashed id from serializer

--- app/serializers/bot_serializer.rb
+++ app/serializers/bot_serializer.rb
@@ -1,5 +1,5 @@
 class BotSerializer < ActiveModel::Serializer
-  attributes  :bot_name, :bot_description, :bot_tags, :current_version, :eth_address, :developer_eth_address, :metadata_url, :hashed_identifier, :latest_transaction_address, :latest_transaction_action, :developer_record_hashed_identifier
+  attributes  :bot_name, :bot_description, :bot_tags, :current_version, :eth_address, :developer_eth_address, :metadata_url, :hashed_identifier, :latest_transaction_address, :latest_transaction_action
 
   def latest_transaction_address
     transaction = object.ethereum_transactions.order(created_at: :desc).first
@@ -11,7 +11,4 @@ class BotSerializer < ActiveModel::Serializer
     transaction.andand.action_name
   end
 
-  def developer_record_hashed_identifier
-    object.developer.andand.developer_record.andand.hashed_identifier
-  end
 end
